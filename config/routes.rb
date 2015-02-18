c7c1e5c5ca87fb52864c5802440fc51e85518cfa
Jason <tangsh@sas.upenn.edu>
2015-02-18 15:43:03 -0500

    more fixes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'test/edit'
 
   get 'test/show'
+  root 'test#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
