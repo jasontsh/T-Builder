a47477b8ff191db7cea8084812d13ba932a3c49c
Jason <tangsh@sas.upenn.edu>
2015-02-18 15:46:47 -0500

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class TestController < ApplicationController
+class UsersController < ApplicationController
   def new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'test/new'
+  get 'users/new'
 
-  get 'test/index'
+  get 'users/index'
 
-  get 'test/edit'
+  get 'users/edit'
 
-  get 'test/show'
-  root 'test#index'
+  get 'users/show'
+
+  root 'users#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
