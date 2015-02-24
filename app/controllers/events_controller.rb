bcc8a9e7a2c0ac43610b30a90bfd8ce5f1f6cd26
Jason <tangsh@sas.upenn.edu>
2015-02-24 09:45:32 -0500

    hw4 second table

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,8 +3,44 @@ class EventsController < ApplicationController
   	@event = Event.new
   end
 
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to events_path
+    else
+      render 'new'
+  end
 
   def index
   	@event = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update_attributes(event_params)
+      redirect_to event_path(@event.id)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@
 #
 
 class User < ActiveRecord::Base
+	validates_presence_of :name
+	validates_length_of :password, :minimum => 8
 end
